ecc5423d418062687b902cdb9955746b34cbeaa8
vherasymenko <herasymenkoveronika@gmail.com>
2017-10-07 01:48:24 +0300

    * Initial commit.

---
+++ src/main/java/com/vacation_bot/VacationBotApplication.java
@@ -0,0 +1,12 @@
+package com.vacation_bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VacationBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VacationBotApplication.class, args);
+	}
+}
