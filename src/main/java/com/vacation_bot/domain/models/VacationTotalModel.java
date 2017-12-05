3344d7294e8935d9388b0342b3ebcdecad90c3b1
Nazar Yunko <yunko.nazar@gmail.com>
2017-12-06 00:11:53 +0200

    First task

---
+++ src/main/java/com/vacation_bot/domain/models/VacationTotalModel.java
@@ -0,0 +1,50 @@
+package com.vacation_bot.domain.models;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.CompoundIndex;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+
+@Getter
+@Setter
+@EqualsAndHashCode
+@Document
+@CompoundIndex(name = "unique_idx", def = "{ 'user-id': 1, 'year': 1 }", unique = true)
+public class VacationTotalModel {
+
+    /**
+     * Database generated primary key of the document.
+     */
+    @Id
+    @Field(FieldNames.ID)
+    private String id;
+
+    /**
+     * The user unique identifier.
+     */
+    @Field(FieldNames.USER_ID)
+    private String userId;
+
+    /**
+     * The total amount of valid vacation days that the user still have this year.
+     */
+    @Field(FieldNames.VACATION_TOTAL)
+    private int vacationTotal;
+
+    /**
+     * The year this document belongs to.
+     */
+    @Field(FieldNames.YEAR)
+    private int year;
+
+    public static class FieldNames {
+        public static final String ID = "_id";
+        public static final String USER_ID = "user-id";
+        public static final String VACATION_TOTAL = "vacation-total";
+        public static final String YEAR = "year";
+    }
+
+}
