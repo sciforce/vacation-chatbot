49eb389adec0e3480fca25c6ee4226088435daa8
Nazar Yunko <yunko.nazar@gmail.com>
2017-12-06 00:42:50 +0200

    Added vacation service

---
+++ src/main/java/com/vacation_bot/core/vacation/VacationService.java
@@ -0,0 +1,7 @@
+package com.vacation_bot.core.vacation;
+
+public interface VacationService {
+
+    String createVacation(String userName, String startDate, String endDate);
+
+}
---
+++ src/main/java/com/vacation_bot/core/vacation/VacationServiceImpl.java
@@ -0,0 +1,12 @@
+package com.vacation_bot.core.vacation;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class VacationServiceImpl implements VacationService{
+
+    @Override
+    public String createVacation(String userName, String startDate, String endDate) {
+        return null;
+    }
+}
